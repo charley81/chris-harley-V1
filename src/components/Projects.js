a66bc7fa2a69b61e5b91d5b36693ce48b4b2ebeb
ChrisKnowsDev <chrisharley81@gmail.com>
2020-11-06 17:30:27 -0500

    added product contenct to strapi, created projects and project component, queried project data in index page and passed data as props

---
+++ src/components/Projects.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Title from './Title'
+import Project from './Project'
+import { Link } from 'gatsby'
+
+export default function Projects({ projects, title, showLink }) {
+  return (
+    <div>
+      <section>
+        <Title title={title} name="title" />
+        <div className="section-center">
+          {projects.map((project, index) => (
+            <Project key={project.id} index={index} />
+          ))}
+        </div>
+        {showLink && (
+          <Link to="/projects" className="btn center-btn">
+            Projects
+          </Link>
+        )}
+      </section>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,47 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import Hero from '../components/Hero'
 import Jobs from '../components/Jobs'
 import Layout from '../components/Layout'
 import Services from '../components/Services'
+import Projects from '../components/Projects'
 
-export default function Home() {
+export default function Home({ data }) {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
+
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
+      <Projects projects={projects} title="Featured Projects" showLink={true} />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        description
+        github
+        id
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
