f94179bf3ac38c67505d107b82e5b1169cb07a0b
ChrisKnowsDev <chrisharley81@gmail.com>
2020-11-13 14:24:19 -0500

    removed about links

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import Layout from '../components/Layout'
+import { Link } from 'gatsby'
+import { css } from '@emotion/core'
 
 export default function FourOhFour() {
   return (
-    <div>
-      <h1>Page Not Found</h1>
-    </div>
+    <Layout>
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          text-align: center;
+          margin-top: 20vh;
+
+          a {
+            color: var(--primaryColor);
+            transition: var(--transition);
+
+            &:hover {
+              color: var(--darkColor);
+            }
+          }
+        `}
+      >
+        <h1>Page Not Found</h1>
+        <Link to="/">Back Home</Link>
+      </div>
+    </Layout>
   )
 }
